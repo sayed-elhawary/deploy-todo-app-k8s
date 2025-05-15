f3841f3c63235f8b2ec10853ced8606b0857c449
Sayed Elhawary <selhawary2025@hotmail.com>
2025-05-16 00:52:34 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
 
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>📝 To-Do List with Jenkins piline </h1>
+      <h1>📝 To-Do List app </h1>
       <input
         type="text"
         value={text}
